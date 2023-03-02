afd26a9be487bf4e3a47ea6c1c4392462fd9b96e
Nicolas_Vargas <nikolas210680@gmail.com>
2023-03-01 20:16:20 -0700

    simple committed

---
+++ snow-shovelling.js
@@ -0,0 +1,43 @@
+const isShovellingTime = true;
+const hasShovel = false;
+const needHelp = true;
+const friendAnswer = false;
+
+if (isShovellingTime) {
+  console.log("Look for the shovel");
+
+  if (hasShovel) {
+    console.log("it is time to work your ass off");
+    console.log("shovel the snow");
+    console.log("Get back inside the house");
+    console.log("get a hot drink");
+  } else {
+    console.log("Need a shovel");
+    console.log("Go to Home Depot");
+    console.log("Get a Shovel");
+    console.log("Drive back Home");
+    console.log("shovel the snow");
+    console.log("Get back inside the house");
+    console.log("get a hot drink");
+  }
+
+  if (needHelp) {
+    console.log("call a friend");
+    console.log("Go to home depot");
+    console.log("buy two shovels");
+    console.log("Go back home and shovel snow");
+    console.log("Get back inside the house");
+    console.log("get a hot drink with your friend");
+
+  } else {
+    console.log("screw your friend");
+    console.log("No need, isn't that terrific");
+    console.log("Go to home depot");
+    console.log("Drive back Home");
+    console.log("shovel the snow");
+    console.log("Get back inside the house");
+    console.log("get a hot drink");
+  }
+  
+} else {
+ console.log("No need, isn't that terrific");
